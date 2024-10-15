4742c19df659cbf643110580fef581e6badfc2c6
Ghanshyam Jha <168712839+GhanshyamJha05@users.noreply.github.com>
2024-10-15 14:36:32 +0530

    to-do list.py

--- to-do list.py
+++ to-do list.py
@@ -1,47 +1,52 @@
 def display_menu():
     print("\nTo-Do List Application")
     print("1. Add Task")
-    print("2. View Tasks")
+    print("2. View Task")
     print("3. Remove Task")
     print("4. Exit")
 
 def main():
-    todo_list = []
-
+    todo_list=[]
     while True:
         display_menu()
-        choice = input("Choose an option (1-4): ")
-
-        if choice == '1':
-            task = input("Enter the task: ")
+        choice=input("Choose an option (1-4):")
+        
+        if choice=='1':
+            task=input("Enter the task:")
             todo_list.append(task)
+            print("--------------------")
             print(f"Task '{task}' added!")
-
-        elif choice == '2':
+            print("--------------------")
+        elif choice=='2':
             if not todo_list:
-                print("Your to-do list is empty.")
+                print("your todo_list is empty!")
             else:
                 print("\nYour Tasks:")
-                for index, task in enumerate(todo_list, start=1):
-                    print(f"{index}. {task}")
-
-        elif choice == '3':
+                print("--------------------")
+                for index, task in enumerate(todo_list,start=1):
+                    print(f"{index}.{task}")
+            print("--------------------")
+        
+        elif choice=='3':
             if not todo_list:
-                print("Your to-do list is empty.")
+                print('your todo-list is empty!')
             else:
-                task_number = int(input("Enter the task number to remove: "))
-                if 1 <= task_number <= len(todo_list):
-                    removed_task = todo_list.pop(task_number - 1)
-                    print(f"Task '{removed_task}' removed!")
+                task_number=int(input("Enter the task number to remove:"))
+                if 1<=task_number<=len(todo_list):
+                    removed_task=todo_list.pop(task_number-1)
+                    print("--------------------")
+                    print(f"Task '{removed_task}'removed!")
+                    print("--------------------")
                 else:
-                    print("Invalid task number.")
-
-        elif choice == '4':
-            print("Exiting the To-Do List Application. Goodbye!")
+                    print("Invalid Task Number!!")
+        
+        elif choice=='4':
+            print("**************************")
+            print("Exiting the TO-DO List application.")
+            print("**************************")
+            print("Thank You!!")
             break
-
         else:
-            print("Invalid choice. Please choose a valid option.")
-
-if __name__ == "__main__":
+            print('Invalid choice. Please choose a valid option.')
+if __name__=="__main__":
     main()
